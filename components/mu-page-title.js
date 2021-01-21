11f95874925fc8369361395ddc3d96b7fcb19477
Christopher McComas <cmccomas@marshall.edu>
2021-01-21 16:06:05 -0500

    fix mobile menu

--- components/mu-page-title.js
+++ components/mu-page-title.js
@@ -1,12 +1,19 @@
 import {LitElement, html, css} from 'lit-element';
 
 class MuPageTitle extends LitElement {
+	static get properties() { return {
+    hidden: { type: Boolean },
+  };}
 
+	constructor() {
+    super();
+    this.hidden = false;
+	}
 
   render() {
 		return html`
 		<link rel="stylesheet" href="//www.marshall.edu/wp-content/themes/marsha/css/marsha2.0.css">
-		<h1><slot class="entry-title font-sans uppercase font-semibold text-gray-700 mb-4 text-3xl lg:text-4xl"></slot></h1>
+		<h1><slot class="entry-title font-sans uppercase font-semibold text-gray-700 mb-4 text-3xl lg:text-4xl ${this.hidden === true ? ' hidden ' : ''}" ></slot></h1>
 		`;
 	}
 }
